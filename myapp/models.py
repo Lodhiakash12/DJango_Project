dbd55ad7d1966433190e6a68f6c210e0e758728b
Lodhiakash12 <akashlodhi7836@gmail.com>
2025-12-01 02:13:51 -0800

    DJango Project

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    fname=models.CharField(max_length=100)
+    lname=models.CharField(max_length=100)
+    mobile=models.PositiveBigIntegerField()
+    profile_picture=models.ImageField(upload_to="profile_picture/")
+    address=models.TextField()
+    email=models.EmailField()
+    password=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.fname+""+self.lname
+
+    
+
--- myapp/views.py
+++ myapp/views.py
@@ -1,16 +1,43 @@
 from django.shortcuts import render
+from .models import User
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
 def contact(request):
     return render(request,'contact.html')
 
 def category(request):
     return render(request,'category.html')
 
-def login(request):
-    return render(request,'login.html')
+ 
 
 def signup(request):
-    return render(request,'signup.html')
+    if request.method=="POST":
+         try:
+             User.objects.get(email=request.POST['email'])
+             msg="Email Already Registered"
+             return render(request,'signup.html',{'msg':msg})
+         except User.DoesNotExist:
+             if request.POST['password']==request.POST['cpassword']:
+                 User.objects.create(
+                     fname=request.POST['fname'],
+                     lname=request.POST['lname'],
+                     email=request.POST['email'],
+                     mobile=request.POST['mobile'],
+                     address=request.POST['address'],
+                     password=request.POST['password'],
+                     profile_picture=request.FILES.get('profile_picture')
+                 )
+                 msg="User Signup Successful"
+                 return render(request,'signup.html',{'msg':msg})
+             else:
+                 msg="Pass & Cpass Doesnt Match"
+                 return render(request,'signup.html',{'msg':msg})
+    else:
+        return render(request,'signup.html')
+    
+
+def login(request):
+    return render(request,'login.html')
